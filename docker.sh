f9a288a87b707d57e46344fed0110fe0710d604a
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2020-11-08 03:03:49 +0300

    reorganize tags

--- docker.sh
+++ docker.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 REV=$(git describe --long --tags --match='v*' --dirty 2>/dev/null || echo dev)
+
 docker build -t kazimsarikaya/csi-sharedhostpath:$REV .
+
+if [ "x$REV" != "xdev" ]; then
+  docker tag kazimsarikaya/csi-sharedhostpath:$REV kazimsarikaya/csi-sharedhostpath:dev-latest
+fi
+
 if [ "x$1" == "xpush" ]; then
-docker push kazimsarikaya/csi-sharedhostpath:$REV
+  docker push kazimsarikaya/csi-sharedhostpath:dev-latest
 fi
