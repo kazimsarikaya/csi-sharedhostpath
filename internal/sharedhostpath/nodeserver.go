eda434063435698efdc71db10161d5d6cadbdd74
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2020-11-22 21:09:02 +0300

    node stage/unstage impl

--- internal/sharedhostpath/nodeserver.go
+++ internal/sharedhostpath/nodeserver.go
@@ -44,8 +44,16 @@ func NewNodeServer(nodeId string, maxVolumesPerNode int64, vh *VolumeHelper) *no
 	return &nodeServer{
 		nodeID:            nodeId,
 		maxVolumesPerNode: maxVolumesPerNode,
-		caps:              []*csi.NodeServiceCapability{},
-		vh:                vh,
+		caps: []*csi.NodeServiceCapability{
+			&csi.NodeServiceCapability{
+				Type: &csi.NodeServiceCapability_Rpc{
+					Rpc: &csi.NodeServiceCapability_RPC{
+						Type: csi.NodeServiceCapability_RPC_STAGE_UNSTAGE_VOLUME,
+					},
+				},
+			},
+		},
+		vh: vh,
 	}
 }
 
@@ -67,20 +75,23 @@ func (ns *nodeServer) NodeGetCapabilities(ctx context.Context, req *csi.NodeGetC
 	}, nil
 }
 
-func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublishVolumeRequest) (*csi.NodePublishVolumeResponse, error) {
-	if req.GetVolumeCapability() == nil {
-		return nil, status.Error(codes.InvalidArgument, "Volume capability missing in request")
+func (ns *nodeServer) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRequest) (*csi.NodeStageVolumeResponse, error) {
+	volumeId := req.GetVolumeId()
+	if len(volumeId) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "NodeStageVolume Volume ID must be provided")
 	}
-	if len(req.GetVolumeId()) == 0 {
-		return nil, status.Error(codes.InvalidArgument, "Volume ID missing in request")
-	}
-	if len(req.GetTargetPath()) == 0 {
-		return nil, status.Error(codes.InvalidArgument, "Target path missing in request")
-	}
-	targetPath := req.GetTargetPath()
-	volumeID := req.GetVolumeId()
 
-	vol, err := ns.vh.GetVolume(volumeID)
+	stagingTargetPath := req.GetStagingTargetPath()
+	if len(stagingTargetPath) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "NodeStageVolume Staging Target Path must be provided")
+	}
+
+	cap := req.GetVolumeCapability()
+	if cap == nil {
+		return nil, status.Error(codes.InvalidArgument, "NodeStageVolume Volume Capability must be provided")
+	}
+
+	vol, err := ns.vh.GetVolume(volumeId)
 	if err != nil {
 		return nil, status.Error(codes.NotFound, err.Error())
 	}
@@ -89,9 +100,8 @@ func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 
 	if req.GetVolumeCapability().GetBlock() != nil {
 		if !vol.IsBlock {
-			return nil, status.Error(codes.InvalidArgument, "cannot publish a non-block volume as block volume")
+			return nil, status.Error(codes.InvalidArgument, "cannot stage a non-block volume as block volume")
 		}
-
 		volPathHandler := volumepathhandler.VolumePathHandler{}
 
 		// Get loop device from the volume path.
@@ -101,15 +111,15 @@ func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 		}
 
 		// Check if the target path exists. Create if not present.
-		_, err = os.Lstat(targetPath)
+		_, err = os.Lstat(stagingTargetPath)
 		if os.IsNotExist(err) {
 			var f *os.File
-			f, err = os.OpenFile(targetPath, os.O_CREATE, 0640)
+			f, err = os.OpenFile(stagingTargetPath, os.O_CREATE, 0640)
 			if err != nil {
-				return nil, status.Error(codes.Internal, fmt.Sprintf("failed to create target path: %s: %v", targetPath, err))
+				return nil, status.Error(codes.Internal, fmt.Sprintf("failed to create target path: %s: %v", stagingTargetPath, err))
 			}
 			if err := f.Close(); err != nil {
-				return nil, status.Error(codes.Internal, fmt.Sprintf("failed to create target path: %s: %v", targetPath, err))
+				return nil, status.Error(codes.Internal, fmt.Sprintf("failed to create target path: %s: %v", stagingTargetPath, err))
 			}
 		}
 		if err != nil {
@@ -117,22 +127,18 @@ func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 		}
 
 		// Check if the target path is already mounted. Prevent remounting.
-		notMount, err := mount.IsNotMountPoint(mounter, targetPath)
+		notMount, err := mount.IsNotMountPoint(mounter, stagingTargetPath)
 		if err != nil {
 			if !os.IsNotExist(err) {
-				return nil, status.Errorf(codes.Internal, "error checking path %s for mount: %s", targetPath, err)
+				return nil, status.Errorf(codes.Internal, "error checking path %s for mount: %s", stagingTargetPath, err)
 			}
 			notMount = true
 		}
-		if !notMount {
-			// It's already mounted.
-			klog.V(5).Infof("Skipping bind-mounting subpath %s: already mounted", targetPath)
-			return &csi.NodePublishVolumeResponse{}, nil
-		}
-
-		options := []string{"bind"}
-		if err := mounter.Mount(loopDevice, targetPath, "", options); err != nil {
-			return nil, status.Error(codes.Internal, fmt.Sprintf("failed to mount block device: %s at %s: %v", loopDevice, targetPath, err))
+		if notMount {
+			options := []string{"bind"}
+			if err := mounter.Mount(loopDevice, stagingTargetPath, "", options); err != nil {
+				return nil, status.Error(codes.Internal, fmt.Sprintf("failed to mount block device: %s at %s: %v", loopDevice, stagingTargetPath, err))
+			}
 		}
 	} else if req.GetVolumeCapability().GetMount() != nil {
 		volume_context := req.GetVolumeContext()
@@ -142,16 +148,16 @@ func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 			return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("required parameter not found: %s", typeParameter))
 		}
 		if vtype == "disk" && !vol.IsBlock {
-			return nil, status.Error(codes.InvalidArgument, "cannot publish a non-block volume as disk volume")
+			return nil, status.Error(codes.InvalidArgument, "cannot stage a non-block volume as disk volume")
 		}
 		if vtype == "folder" && vol.IsBlock {
-			return nil, status.Error(codes.InvalidArgument, "cannot publish a block volume as folder volume")
+			return nil, status.Error(codes.InvalidArgument, "cannot stage a block volume as folder volume")
 		}
 
-		notMnt, err := mount.IsNotMountPoint(mounter, targetPath)
+		notMnt, err := mount.IsNotMountPoint(mounter, stagingTargetPath)
 		if err != nil {
 			if os.IsNotExist(err) {
-				if err = os.MkdirAll(targetPath, 0750); err != nil {
+				if err = os.MkdirAll(stagingTargetPath, 0750); err != nil {
 					return nil, status.Error(codes.Internal, err.Error())
 				}
 				notMnt = true
@@ -160,43 +166,143 @@ func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 			}
 		}
 
-		if !notMnt {
-			return &csi.NodePublishVolumeResponse{}, nil
+		if notMnt {
+			options := []string{}
+
+			if vtype == "folder" {
+				options = append(options, "bind")
+				if err := mounter.Mount(vol.VolPath, stagingTargetPath, "", options); err != nil {
+					return nil, status.Error(codes.Internal, fmt.Sprintf("failed to mount device: %s at %s: %s", vol.VolPath, stagingTargetPath, err.Error()))
+				}
+			} else if vtype == "disk" {
+				var fsType string
+				if fsType, found = volume_context[fstypeParameter]; !found {
+					return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("required parameter not found: %s", fstypeParameter))
+				}
+				if fsType == "xfs" {
+					options = append(options, "nouuid")
+				}
+				volPathHandler := volumepathhandler.NewBlockVolumePathHandler()
+				loopDevice, err := volPathHandler.AttachFileDevice(vol.VolPath)
+				if err != nil {
+					return nil, status.Error(codes.Internal, fmt.Sprintf("cannot create loop device: %s", err.Error()))
+				}
+				formatAndMount := mount.SafeFormatAndMount{Interface: mounter, Exec: utilexec.New()}
+				err = formatAndMount.FormatAndMount(loopDevice, stagingTargetPath, fsType, options)
+				if err != nil {
+					return nil, status.Error(codes.Internal, fmt.Sprintf("failed to mount device: %s at %s: %s", vol.VolPath, stagingTargetPath, err.Error()))
+				}
+			} else {
+				return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("invalid volume type: %s", vtype))
+			}
 		}
 
+	}
+
+	return &csi.NodeStageVolumeResponse{}, nil
+}
+
+func (ns *nodeServer) NodeUnstageVolume(ctx context.Context, req *csi.NodeUnstageVolumeRequest) (*csi.NodeUnstageVolumeResponse, error) {
+	volumeId := req.GetVolumeId()
+	if len(volumeId) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "NodeStageVolume Volume ID must be provided")
+	}
+
+	stagingTargetPath := req.GetStagingTargetPath()
+	if len(stagingTargetPath) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "NodeStageVolume Staging Target Path must be provided")
+	}
+
+	vol, err := ns.vh.GetVolume(volumeId)
+	if err != nil {
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	if notMnt, err := mount.IsNotMountPoint(mount.New(""), stagingTargetPath); err != nil {
+		if !os.IsNotExist(err) {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+	} else if !notMnt {
+		err = mount.New("").Unmount(stagingTargetPath)
+		if err != nil {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+	}
+
+	if vol.IsBlock {
+		volPathHandler := volumepathhandler.NewBlockVolumePathHandler()
+		err := volPathHandler.DetachFileDevice(vol.VolPath)
+		if err != nil {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+	}
+
+	if err = os.RemoveAll(stagingTargetPath); err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	klog.V(5).Infof("hostpath: volume %s has been unstaged.", stagingTargetPath)
+
+	return &csi.NodeUnstageVolumeResponse{}, nil
+}
+
+func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublishVolumeRequest) (*csi.NodePublishVolumeResponse, error) {
+	cap := req.GetVolumeCapability()
+	if cap == nil {
+		return nil, status.Error(codes.InvalidArgument, "NodePublishVolume Volume capability missing in request")
+	}
+
+	volumeID := req.GetVolumeId()
+	if len(volumeID) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "NodePublishVolume Volume ID missing in request")
+	}
+
+	targetPath := req.GetTargetPath()
+	if len(targetPath) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "NodePublishVolume Target path missing in request")
+	}
+
+	stagingTargetPath := req.GetStagingTargetPath()
+	if len(stagingTargetPath) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "NodePublishVolume Staging Target Path must be provided")
+	}
+
+	vol, err := ns.vh.GetVolume(volumeID)
+	if err != nil {
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	if cap.GetBlock() != nil {
+		if !vol.IsBlock {
+			return nil, status.Error(codes.InvalidArgument, "cannot publish a non-block volume as block volume")
+		}
+	}
+
+	mounter := mount.New("")
+
+	notMnt, err := mount.IsNotMountPoint(mounter, targetPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			if err = os.MkdirAll(targetPath, 0750); err != nil {
+				return nil, status.Error(codes.Internal, err.Error())
+			}
+			notMnt = true
+		} else {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+	}
+
+	if notMnt {
+		options := []string{"bind"}
+
 		readOnly := req.GetReadonly()
-		options := []string{}
 		if readOnly {
 			options = append(options, "ro")
 		}
 
-		if vtype == "folder" {
-			options = append(options, "bind")
-			if err := mounter.Mount(vol.VolPath, targetPath, "", options); err != nil {
-				return nil, status.Error(codes.Internal, fmt.Sprintf("failed to mount device: %s at %s: %s", vol.VolPath, targetPath, err.Error()))
-			}
-		} else if vtype == "disk" {
-			var fsType string
-			if fsType, found = volume_context[fstypeParameter]; !found {
-				return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("required parameter not found: %s", fstypeParameter))
-			}
-			if fsType == "xfs" {
-				options = append(options, "nouuid")
-			}
-			volPathHandler := volumepathhandler.NewBlockVolumePathHandler()
-			loopDevice, err := volPathHandler.AttachFileDevice(vol.VolPath)
-			if err != nil {
-				return nil, status.Error(codes.Internal, fmt.Sprintf("cannot create loop device: %s", err.Error()))
-			}
-			formatAndMount := mount.SafeFormatAndMount{Interface: mounter, Exec: utilexec.New()}
-			err = formatAndMount.FormatAndMount(loopDevice, targetPath, fsType, options)
-			if err != nil {
-				return nil, status.Error(codes.Internal, fmt.Sprintf("failed to mount device: %s at %s: %s", vol.VolPath, targetPath, err.Error()))
-			}
-		} else {
-			return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("invalid volume type: %s", vtype))
+		if err := mounter.Mount(stagingTargetPath, targetPath, "", options); err != nil {
+			return nil, status.Error(codes.Internal, fmt.Sprintf("failed to mount block device: %s at %s: %v", stagingTargetPath, targetPath, err))
 		}
-
 	}
 
 	return &csi.NodePublishVolumeResponse{}, nil
@@ -213,7 +319,7 @@ func (ns *nodeServer) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpu
 	targetPath := req.GetTargetPath()
 	volumeID := req.GetVolumeId()
 
-	vol, err := ns.vh.GetVolume(volumeID)
+	_, err := ns.vh.GetVolume(volumeID)
 	if err != nil {
 		return nil, status.Error(codes.NotFound, err.Error())
 	}
@@ -229,14 +335,6 @@ func (ns *nodeServer) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpu
 		}
 	}
 
-	if vol.IsBlock {
-		volPathHandler := volumepathhandler.NewBlockVolumePathHandler()
-		err := volPathHandler.DetachFileDevice(vol.VolPath)
-		if err != nil {
-			return nil, status.Error(codes.Internal, err.Error())
-		}
-	}
-
 	if err = os.RemoveAll(targetPath); err != nil {
 		return nil, status.Error(codes.Internal, err.Error())
 	}
@@ -247,14 +345,6 @@ func (ns *nodeServer) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpu
 }
 
 /* Unimplemented methods beyond */
-
-func (ns *nodeServer) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRequest) (*csi.NodeStageVolumeResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "")
-}
-
-func (ns *nodeServer) NodeUnstageVolume(ctx context.Context, req *csi.NodeUnstageVolumeRequest) (*csi.NodeUnstageVolumeResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "")
-}
 
 func (ns *nodeServer) NodeGetVolumeStats(ctx context.Context, in *csi.NodeGetVolumeStatsRequest) (*csi.NodeGetVolumeStatsResponse, error) {
 	return nil, status.Error(codes.Unimplemented, "")
