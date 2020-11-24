1f5bc046b7db2d3e9032225e59167a88ef078607
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2020-11-24 16:21:49 +0300

    add cover and improve running time
    Add coverage percent to the test and change port waiting loop at
    before running test
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- test.sh
+++ test.sh
@@ -4,9 +4,6 @@ rm -fr tmp/*
 if [ "x$1" == "xrun" ]; then
   docker rm -f testdb
   docker run -d --name testdb --rm -e POSTGRES_USER=sharedhostpath  -e POSTGRES_PASSWORD=sharedhostpath -p 5432:5432 postgres:12-alpine
-  while ! nc -z $TARGET_HOST 5432; do
-    sleep 0.5 # wait for 1/2 of the second before check again
-  done
 fi
 
 find . -maxdepth 1 -name "*.test" -delete
@@ -16,10 +13,13 @@ go mod tidy
 go mod vendor
 for pkg in `go list ./...`;
 do
-  go test -c $pkg
+  go test -cover -c $pkg
 done
 
 if [ "x$1" == "xrun" ]; then
+  while ! nc -z $TARGET_HOST 5432; do
+    sleep 0.5 # wait for 1/2 of the second before check again
+  done
   for tf in $(find . -type f -name "*.test");
   do
     ./$tf -dataroot "./tmp/" -dsn "user=sharedhostpath password=sharedhostpath dbname=sharedhostpath port=5432 host=$TARGET_HOST sslmode=disable" -ginkgo.v 9 || exit 1
