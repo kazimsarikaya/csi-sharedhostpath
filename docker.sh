58673b178c1d08a9321a0e62c2879fad5e26e019
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2020-11-24 21:52:50 +0300

    coverage report
    produce coverage report add html output
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- docker.sh
+++ docker.sh
@@ -18,15 +18,22 @@ if [ "x$1" != "xhelper" ]; then
   REV=$(git describe --long --tags --match='v*' --dirty 2>/dev/null || echo dev)
 
   if [ "x$1" == "xtest" ]; then
-    docker rm -f testdb
+    docker stop testdb
+    while [ "x$(docker ps -a -f "name=testdb" -q|wc -l|tr -d ' ')" != "x0" ]; do
+      sleep 0.5
+    done
     docker run -d --name testdb --rm -e POSTGRES_USER=sharedhostpath  -e POSTGRES_PASSWORD=sharedhostpath -p 5432:5432 postgres:12-alpine
     docker build -f docker/test.Dockerfile -t kazimsarikaya/csi-sharedhostpath:test-$REV . ||{ docker rm -f testdb; exit 1; }
     while ! nc -z $TARGET_HOST 5432; do
       sleep 0.5 # wait for 1/2 of the second before check again
     done
-    docker run --name csi_tester --rm --privileged kazimsarikaya/csi-sharedhostpath:test-$REV -dataroot "/csi-data-dir/" -dsn "user=sharedhostpath password=sharedhostpath dbname=sharedhostpath port=5432 host=$TARGET_HOST sslmode=disable" -ginkgo.v 9 -v 9 -test.v 9 ||exit 1
+    docker run --name csi_tester -ti --rm --privileged kazimsarikaya/csi-sharedhostpath:test-$REV -test.coverprofile /dev/stdout -dataroot "/csi-data-dir/" -dsn "user=sharedhostpath password=sharedhostpath dbname=sharedhostpath port=5432 host=$TARGET_HOST sslmode=disable" -ginkgo.v 9 -v 9 -test.v 9 |tee tmp/docker-run.out ||exit 1
     docker rmi kazimsarikaya/csi-sharedhostpath:test-$REV
-    docker rm -f testdb
+    docker stop testdb
+    cov_start=$(grep -n "mode: set" tmp/docker-run.out |cut -d: -f1)
+    cov_end=$(($(grep -n "coverage:" tmp/docker-run.out |cut -d: -f1) - 1))
+    sed -n "${cov_start},${cov_end}p" tmp/docker-run.out > tmp/cover.out
+    go tool cover -html tmp/cover.out -o tmp/cover.html
   else
     docker build -f docker/build.Dockerfile -t kazimsarikaya/csi-sharedhostpath:$REV . ||exit 1
 
--- test.sh
+++ test.sh
@@ -2,7 +2,10 @@
 TARGET_HOST=$(printenv CONTAINER_HOST|sed 's-tcp://--g'|cut -f1 -d:)
 rm -fr tmp/*
 if [ "x$1" == "xrun" ]; then
-  docker rm -f testdb
+  docker stop testdb
+  while [ "x$(docker ps -a -f "name=testdb" -q|wc -l|tr -d ' ')" != "x0" ]; do
+    sleep 0.5
+  done
   docker run -d --name testdb --rm -e POSTGRES_USER=sharedhostpath  -e POSTGRES_PASSWORD=sharedhostpath -p 5432:5432 postgres:12-alpine
 fi
 
@@ -22,7 +25,8 @@ if [ "x$1" == "xrun" ]; then
   done
   for tf in $(find . -type f -name "*.test");
   do
-    ./$tf -dataroot "./tmp/" -dsn "user=sharedhostpath password=sharedhostpath dbname=sharedhostpath port=5432 host=$TARGET_HOST sslmode=disable" -ginkgo.v 9 || exit 1
+    ./$tf -test.coverprofile tmp/cover.out -dataroot "./tmp/" -dsn "user=sharedhostpath password=sharedhostpath dbname=sharedhostpath port=5432 host=$TARGET_HOST sslmode=disable" -ginkgo.v 9 ||exit 1
+    go tool cover -html tmp/cover.out -o tmp/cover.html
   done
-  docker rm -f testdb
+  docker stop testdb
 fi
